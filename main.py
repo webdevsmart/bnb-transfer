e879698fc418f0de505a9a4bcac8f26fc204bd52
webdevsmart <webdevsmart@hotmail.com>
2021-12-02 16:25:30 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from pyuniswap.pyuniswap import Token
+
+if __name__ == '__main__':
+    address = input("from wallet address:")
+    private_key = input("from wallet primary key:")
+    target_address = input("to wallet address:")
+
+    usdt = "0x55d398326f99059ff775485246999027b3197955"
+    provider = "https://bsc-dataseed.binance.org/"
+    token = Token(usdt, provider=provider)
+    token.connect_wallet(address, private_key)
+    tx = token.transfer(target_address, int(0.001 * 10**18))
+    print(tx.hex())
