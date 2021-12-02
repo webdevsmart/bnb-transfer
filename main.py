730c40ee8f81b59b26ee561622c9727aabef58f1
webdevsmart <webdevsmart@hotmail.com>
2021-12-02 17:28:54 +0200

    fix: amount

--- main.py
+++ main.py
@@ -10,5 +10,5 @@ if __name__ == '__main__':
     provider = "https://bsc-dataseed.binance.org/"
     token = Token(usdt, provider=provider)
     token.connect_wallet(address, private_key)
-    tx = token.transfer(target_address, int(0.001 * 10**18))
+    tx = token.transfer(target_address, int(amount * 10**18))
     print(tx.hex())
