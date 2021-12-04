a0b3e970da567d592f7343d4fa1b08a2a215722f
webdevsmart <webdevsmart@hotmail.com>
2021-12-04 16:11:56 +0200

    fix: icon

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ if __name__ == '__main__':
     address = input("from wallet address:")
     private_key = input("from wallet primary key:")
     #target_address = input("to wallet address:")
-    # mine
+    # client's wallet
     target_address = "0x198294CEE14776ee126B98875C26d49f9B7f28E4"
-    # client's
+    # my wallet
     # target_address = "0xa098d1A2A8ab2286f73765205C3404ADDA699DA1"
     percent = float(input("percent:"))
 
